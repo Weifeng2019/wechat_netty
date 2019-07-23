d49a53b2ac6596dc01996ac554e0fc05cad360b7
Weifeng <2448078149@qq.com>
2019-07-23 11:15:41 +0800

    Initial commit

---
+++ src/main/java/the/flash/attribute/Attributes.java
@@ -0,0 +1,10 @@
+package the.flash.attribute;
+
+import io.netty.util.AttributeKey;
+import the.flash.session.Session;
+
+public interface Attributes {
+    AttributeKey<Session> SESSION = AttributeKey.newInstance("session");
+}
+
+
---
+++ src/main/java/the/flash/client2/console/ConsoleCommand.java
@@ -0,0 +1,9 @@
+package the.flash.client2.console;
+
+import io.netty.channel.Channel;
+
+import java.util.Scanner;
+
+public interface ConsoleCommand {
+    void exec(Scanner scanner, Channel channel);
+}
